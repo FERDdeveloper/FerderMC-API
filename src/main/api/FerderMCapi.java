7a8d0beea43f2a39486adea30338ea24eaea6a56
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-10-03 18:36:03 +0200

    ll

---
+++ src/main/api/FerderMCapi.java
@@ -0,0 +1,15 @@
+package main.api;
+
+public class FerderMCapi {
+
+    @override public static l
+    {
+        String api = new String();
+
+    void main(String[api], args) {
+
+    int api() {
+                int source = "";
+                int server = mc.getServer();
+            }
+}}}
---
+++ src/main/java/mod/id/modname/mod.java
@@ -0,0 +1,15 @@
+package main.java.mod.id.modname;
+
+import net.minecraft.*;
+
+public class modnmae extends minecraft {
+
+    @override
+    String o = new String();
+
+    public static void main(String[] args) {
+        /*
+         * code goes here
+         */
+    }
+}
